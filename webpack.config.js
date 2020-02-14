31f15e3b77214e1581fd57a311c3a9b0c4d18532
SimonLi <chenmoonmo@gmail.com>
2020-02-14 19:33:08 +0800

    open

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlplugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html'
+});
 
 module.exports = {
-    mode:'development',
+    mode: 'development',
     // mode: 'production'
-    entry: path.join(__dirname,'./src/index.js'),
+    entry: path.join(__dirname, './src/index.js'),
     output: {
-        path: path.join(__dirname,'./dist'),
+        path: path.join(__dirname, './dist'),
         filename: 'bundle.js'
-    }
+    },
+    plugins: [htmlplugin]
 }
