9c5156b10bdd0b55e2730b123d06a44728e46e65
SimonLi <chenmoonmo@gmail.com>
2020-02-14 20:40:02 +0800

    css

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,11 @@ module.exports = {
         path: path.join(__dirname, './dist'),
         filename: 'bundle.js'
     },
-    plugins: [htmlplugin]
+    plugins: [htmlplugin],
+    module: {
+        rules: [
+            {test: /\.css$/,use:['style-loader','css-loader']},
+            {test:/\.less$/,use: ['style-loader','css-loader','less-loader']}
+        ]
+    }
 }
